39efe67d6700dcbefc3247b7b7281e2d9bcd97cf
Aswaglord <andrew@blonquist.com>
2021-04-08 21:38:05 -0400

    pushed to main

--- index.js
+++ index.js
@@ -133,9 +133,6 @@ function rowsGood(board) {
     return true;
 }
 
-
-
-
 function columnGood(board) {
     for (var i = 0; i < 9; i++) {
         var cur = []
@@ -177,23 +174,18 @@ function boxesGood(board) {
         return true;
     }
 
-    //console.log(solve(test1));
-    //console.log(solve(test2));
-
-
-
 let testing = () => {
     let board = document.querySelectorAll("input");
-    let nums = []
+    let nums = [];
+    
     for (let i = 0; i < board.length; i++) {
         if (board[i].value === "") {
             nums.push(null);
         } else {
             nums.push(parseInt(board[i].value));
         }
-        
     }
-
+    
     let twoD = []
     while(nums.length) twoD.push(nums.splice(0,9));
     
@@ -202,16 +194,54 @@ let testing = () => {
     for (let i = 0; i < solution.length; i++) {
         oneD = oneD.concat(solution[i]); 
     }
-    
-    console.log({twoD});
-    console.log({oneD});
-    console.log({solution});
     for (let i = 0; i < oneD.length; i++) {
-        board[i].value = oneD[i]
-        
+        board[i].value = oneD[i] 
     }
-    
-    
 }
 
+    let hint = () => {
+        let preBoard = document.querySelectorAll("input");
+        let inputsToArr = [];
+
+        for (let i = 0; i < preBoard.length; i++) {
+            if (preBoard[i].value === "") {
+                inputsToArr.push(null);
+            } else {
+                inputsToArr.push(parseInt(preBoard[i].value));
+            }
+        }
+
+        let twoD = []
+        while(inputsToArr.length) twoD.push(inputsToArr.splice(0,9));
+
+        let unsolvedOneD = [];
+        let solved = solve(twoD);
+        let oneD = [];
+
+        for (let i = 0; i < solved.length; i++) {
+            unsolvedOneD = unsolvedOneD.concat(twoD[i]);
+        }
+        
+        for (let i = 0; i < solved.length; i++) {
+            oneD = oneD.concat(solved[i]); 
+        }
+        
+        for (let i = 0; i < unsolvedOneD.length; i++) {
+            if (unsolvedOneD[i] === null) {
+                preBoard[i].value = oneD[i];
+                break;
+            }
+        }
+    }
+
+    let reset = () => {
+       let board = document.querySelectorAll("input")
+
+       for (let i = 0; i < board.length; i++) {
+           board[i].value = ""
+       }
+    }
+
 document.querySelector(".solve").addEventListener("click", testing);
+document.querySelector(".hint").addEventListener("click", hint);
+document.querySelector(".reset").addEventListener("click", reset);
